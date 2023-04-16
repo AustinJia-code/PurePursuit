107174c1ccbaef24c57a5372fb7b33f7d13fdc1c
Austin Jia <56139100+AustinJia-code@users.noreply.github.com>
2023-04-16 12:18:49 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftPID.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
+
+public class LiftPID {
+    private double kp, ki, kd, ogP, ogI;
+    private double totalError, lastError, lastTime;
+    private ArrayList<Double> errors;
+    private int target, max;
+    private boolean isClose;
+    private double I2CDeadzone = 30;
+    private double deadzone = 5;
+
+    public LiftPID(double kp, double ki, double kd, int target, int max) {
+        this.ogP = kp;
+        this.kp = kp;
+        this.ogI = ki;
+        this.ki = ki;
+        this.kd = kd;
+        lastTime = 0;
+        this.target = target;
+        this.max = max;
+        errors = new ArrayList<Double>();
+    }
+
+    public double getP(double error) {
+        return kp * error;
+    }
+
+    public double getI(double error) {
+        if (Math.abs(error) < 0.001) totalError = 0;
+        return ki * totalError;
+    }
+
+    public double getD(double currentError) {
+        return kd * (currentError - lastError);
+    }
+
+    public boolean checkCorrection(double error){
+
+        if(Math.abs(errors.get(errors.size()-1)) < Math.abs(error))
+            return true;
+        else
+            return false;
+
+    }
+    public void setP(double p){
+        kp = p;
+    }
+    public void setI(double i){ ki = i;}
+
+    public double getCorrection(double error) {
+        if (Math.abs(error) < 0.001) {
+            return 0;
+        }
+
+        totalError += error;
+
+        double output = getP(error) + getI(error) + getD(error);
+
+        lastError = error;
+        errors.add(output);
+
+
+        return output;
+    }
+
+    public void clearError(){
+        totalError = 0;
+    }
+
+    public double getCorrectionPosition(double position, double voltage){
+        setP(ogP / (13.8 / voltage));
+
+        if(Math.abs(position-target) < I2CDeadzone) isClose = true;
+        else isClose = false;
+
+        if(Math.abs(position-target) <= deadzone || ((target == 0) && position < target)){
+            totalError = 0;
+            return 0;
+        }
+
+        if((target < 20)) setI(ogI);
+        else setI(0);
+
+        return getCorrection((target-position)/max);
+    }
+
+    public int getTarget(){
+        return target;
+    }
+
+    public boolean isClose(){ return isClose;}
+
+    public ArrayList<Double> aman(){
+        return errors;
+    }
+
+    public void setTarget(int target){
+        this.target = target;
+    }
+}
